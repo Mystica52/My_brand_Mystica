ca9080d070a9a327dfc3d6b98dfba781c3aaa5ad
Mystica <r.dukuzeyez@alustudent.com>
2023-01-14 16:17:56 +0200

    2nd commit

--- Dashboard/js/scripts.js
+++ Dashboard/js/scripts.js
@@ -1,32 +1,7 @@
-var manageBlog=document.querySelector("#blog-btn");
-        function manageBlogFunc(){
-            window.location.href="./index.html"
-
-        }
-        var manageBlog=document.querySelector("#contact-btn");
-        
-
-        function manageContactFunc(){
-            window.location.href="./contact_me.html"
-        }
-
-        function manageLoginFunc(){
-            window.location.href="./control_login.html"
-        }
-
-        var blogModal= document.querySelector(".create-blog-modal");
-        var closeBlogModal= document.querySelector("#close-blog-modal");
-       function blogModalFunc(){
-        blogModal.style.display = "flex";
-        }
-        closeBlogModal.addEventListener("click", closeBlogModalFunc);
-        function closeBlogModalFunc(){
-            blogModal.style.display = "none";
-        }
-
-        window.addEventListener("click", outsideClickBlogModal);
-        function outsideClickBlogModal(e){
-            if(e.target == blogModal){
-                blogModal.style.display = "none";
-            }
-        }
+const toggleButton= document.getElementsByClassName('checkbtn')[0];
+const menuBar= document.getElementsByClassName('navBar-links');
+toggleButton.addEventListener('click', function(){
+    for (var i=0; i<menuBar.length; i++){
+        menuBar[i].classList.toggle('active')
+    }
+})
